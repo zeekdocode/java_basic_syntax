12fa2079ab3cd3d8296fa04f9c4c03a7372cbf4b
zeekdocodes <clydeeeee19@gmail.com>
2025-03-31 12:06:01 +0800

    this are the basic syntax for math equation

---
+++ pMath.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class pMath {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("what is the first number:");
+        // int a = scanner.nextInt();
+        // System.out.println("what is the second number:");
+        // int b = scanner.nextInt();
+        // int c = a + b; // operator are +, /, *, -, %
+        // System.out.println("the sum of the two number is:"+c);
+        // System.out.println("input decimal");
+        // double a = scanner.nextDouble();
+        // System.out.println("input another decimal");
+        // double b = scanner.nextDouble();
+        // System.out.println();
+        // double c = a + b; // float double same operators
+        // System.out.println(c);
+
+        // double resultDbl3 = 0D; // java don't have precision in floating numbers
+        // System.out.println("resultDbl3 = " + resultDbl3);
+
+        // for (int i = 0; i < 100; i++) {
+        // resultDbl3 += 0.01D;
+        // }
+        // System.out.println("resultDbl3 = " + resultDbl3);
+        // double abs = Math.abs(-200.2131231); // int double float long
+        // double ceil = Math.ceil(7.8); floor and ceiling
+
+        // double result3 = Math.floorDiv(-100,9);
+        // System.out.println("result3: " + result3);
+
+        // double result4 = -100 / 9;
+        // System.out.println("result4: " + result4);
+
+        // int min = Math.min(10, 20); min,max
+        // System.out.println(min);
+
+        // double roundedDown = Math.round(23.445);
+        // double roundedUp = Math.round(23.545);
+
+        // System.out.println(roundedDown);
+        // System.out.println(roundedUp);
+
+        // double random = Math.random();
+        // System.out.println(random);
+        // double exp1 = Math.exp(1);
+        // System.out.println("exp1 = " + exp1);
+
+        // double exp2 = Math.exp(2);
+        // System.out.println("exp2 = " + exp2);
+
+        // double a = Math.log(10);
+
+        // System.out.println(a);
+        // double log10_1 = Math.log10(1);
+        // System.out.println("log10_1 = " + log10_1);
+
+        // double log10_100 = Math.log10(100);
+        // System.out.println("log10_100 = " + log10_100);
+
+        // System.out.println("input logs:");
+        // double a = scanner.nextDouble();
+
+        // System.out.println( Math.log10(a));
+
+        // double pow2 = Math.pow(2,2);
+        // System.out.println("pow2 = " + pow2);
+
+        // double pow8 = Math.pow(2,8);
+        // System.out.println("pow8 = " + pow8);
+
+        // double sqrt4 = Math.sqrt(4);
+        // System.out.println("sqrt4 = " + sqrt4);
+
+        // double sqrt9 = Math.sqrt(9);
+        // System.out.println("sqrt9 = " + sqrt9);
+
+        // double sin = Math.sin(Math.PI);
+        // System.out.println("sin = " + sin);
+
+        // double cos = Math.cos(Math.PI);
+        // System.out.println("cos = " + cos);
+
+        // double tan = Math.tan(Math.PI);
+        // System.out.println("tan = " + tan);
+
+        // double asin = Math.asin(1.0);
+        // System.out.println("asin = " + asin);
+
+        // double acos = Math.acos(1.0);
+        // System.out.println("acos = " + acos);
+
+        // double atan = Math.atan(1.0);
+        // System.out.println("atan = " + atan);
+
+        // double sinh = Math.sinh(1.0);
+        // System.out.println("sinh = " + sinh);
+
+        // double cosh = Math.cosh(1.0);
+        // System.out.println("cosh = " + cosh);
+
+        // double tanh = Math.tanh(1.0);
+        // System.out.println("tanh = " + tanh);
+
+        // double degrees = Math.toDegrees(Math.PI);
+        // System.out.println("degrees = " + degrees);
+
+        // double radians = Math.toRadians(180);
+        // System.out.println("radians = " + radians);
+
+        scanner.close();
+    }
+
+}
