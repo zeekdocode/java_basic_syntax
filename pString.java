b071d30d03b69f3a7feb34af5fba9560a6e81abb
zeekdocodes <clydeeeee19@gmail.com>
2025-03-31 11:26:56 +0800

    first trying to commit

---
+++ pString.java
@@ -0,0 +1,126 @@
+import java.nio.charset.Charset;
+import java.util.Scanner;
+import java.nio.charset.Charset;
+
+public class pString {
+    public static void main(String[] args) {
+        // String theString = "is this good or is this bad?";
+        // String substring = "is";
+
+        // // int index = theString.indexOf(substring);
+        // // while(index != -1) {
+        // // System.out.println(index);
+        // // index = theString.indexOf(substring, index + 1);
+        // int index = theString.lastIndexOf(substring);
+        // System.out.println(index);
+
+        // String text = "one two three two one";
+
+        // boolean matches = text.matches(".*two.*");
+        // System.out.println(matches);
+        // String one = "This is a good day to code";
+
+        // System.out.println( one.startsWith("This") );
+        // System.out.println( one.startsWith("This", 5) );
+
+        // System.out.println( one.endsWith ("code") );
+        // System.out.println( one.endsWith ("shower") );
+        // String one = "abc";
+        // String two = "def";
+        // String three = "abd";
+
+        // System.out.println( three.compareTo(two) );
+        // System.out.println( one.compareTo(three) );
+        // String text = " And he ran across the field ";
+        // String trimmed = text.trim();
+        // System.out.println(trimmed);     
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Tell me your name: ");
+        // String name = scanner.nextLine();
+        // String nametrimmed = name.trim();
+
+        // System.out.println(nametrimmed);
+        // scanner.close();
+
+        // String source = "123abc";
+        // String replaced = source.replace('a', '@');
+        // System.out.println(source);
+        // System.out.println(replaced);
+        // String text = "one two three two one";
+
+        // String s = text.replaceFirst("two", "five");
+        // System.out.println(text);
+        // System.out.println(s);
+        // String text = "one two three two one";
+
+        // String t = text.replaceAll("two", "five");
+        // System.out.println(text);
+        // System.out.println(t);
+        // String source = "A man drove with a car.";
+        // int limit = 3;
+        // String[] occurrences = source.split("a", limit);
+        // System.out.println(occurrences.length);
+        // System.out.println("Occurrences:");
+        // for (String part : occurrences) {
+        // System.out.println("\"" + part + "\"");
+
+        // String intStr = String.valueOf(10);
+        // System.out.println("intStr = " + intStr);
+
+        // String flStr = String.valueOf(9.99);
+        // System.out.println("flStr = " + flStr);
+
+        // Integer integer = new Integer(123);
+
+        // String intStr = integer.toString();
+
+        // System.out.println(intStr);
+
+        // String theString = "This is a good day to code";
+
+        // System.out.println( theString.charAt(0) );
+        // System.out.println( theString.charAt(3) );
+        // String theString = "This is a good day to code";
+
+        // byte[] bytes1 = theString.getBytes();
+        // byte[] bytes2 = theString
+        // .getBytes(Charset.forName("UTF-8"));
+
+        // System.out.println(bytes1);
+        // System.out.println(bytes2);
+
+        // String theString =
+        // "This IS a mix of UPPERcase and lowerCASE";
+
+        // String uppercase = theString.toUpperCase();
+        // String lowercase = theString.toLowerCase();
+
+        // System.out.println(uppercase);
+        // System.out.println(lowercase);
+
+        // String input = "Hello %s";
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter first person");
+        // String fname = scanner.nextLine();
+        // System.out.println("Enter second person");
+        // String sname = scanner.nextLine();
+        // String output1 = input.formatted(fname);
+        // System.out.println(output1);
+        // String output2 = input.formatted(sname);
+        // System.out.println(output2);
+        // System.out.println(input);
+        // scanner.close();
+
+        // String input1 = " Hey \n This \n is \n indented.";
+        // String output = input1.stripIndent();
+        // System.out.println(output);
+
+        // String input = "Hey, \\n This is not normally a line break.";
+        // System.out.println(input);
+
+        // String output = input.translateEscapes();
+        // System.out.println(output);
+        // System.out.println("hellow world");
+        
+    }
+}
