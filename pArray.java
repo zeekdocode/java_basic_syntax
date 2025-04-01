61e4f2a37d1bed6303308d12b434fed2c98bd52a
zeekdocodes <clydeeeee19@gmail.com>
2025-04-01 19:29:16 +0800

    this are the basic array syntax

---
+++ pArray.java
@@ -0,0 +1,20 @@
+
+public class pArray {
+    public static void main(String[] args) {
+
+        // int[] intArray; // declaring an array of intereger of any variable type
+        // String[] stringArray;
+
+        // MyClass[] myClassArray;
+        int numA[]  = {1,2,3,4,5};
+
+        System.out.println(numA[2]);
+        
+
+
+    
+    
+    
+    }
+
+}
