c87b4ca190c25be0e5d577b5d2a61ffce96858d6
zeekdocodes <clydeeeee19@gmail.com>
2025-04-02 21:52:21 +0800

    this is the end of the array syntax that i reed

--- pArray.java
+++ pArray.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class pArray {
     public static void main(String[] args) {
@@ -234,21 +235,153 @@ public class pArray {
         // int[] ints = new int[10];
 
         // for (int i = 0; i < ints.length; i++) {
-        //     ints[i] = 10 - i;
+        // ints[i] = 10 - i;
         // }
 
         // System.out.println(java.util.Arrays.toString(ints));
 
         // int[] ints = {20,5,8,100,50,10};
         // for(int i=0; i < ints.length; i++)
-        
+
         // System.out.println(java.util.Arrays.toString(ints));
-        
+
         // java.util.Arrays.sort(ints);
-        
+
         // System.out.println(java.util.Arrays.toString(ints));
-    
-    
+
+        // private static class Employee {
+        // public String name;
+        // public int employeeId;
+
+        // public Employee(String name, int employeeId) {
+        // this.name = name;
+        // this.employeeId = employeeId;
+        // }
+        // }
+        // Employee[] employeeArray = new Employee[3];
+
+        // employeeArray[0] = new Employee("Xander", 1);
+        // employeeArray[1] = new Employee("John", 3);
+        // employeeArray[2] = new Employee("Anna", 2);
+
+        // java.util.Arrays.sort(employeeArray, new Comparator<Employee>() {
+        // @Override
+        // public int compare(Employee e1, Employee e2) {
+        // return e1.name.compareTo(e2.name);
+        // }
+        // });
+
+        // for (int i = 0; i < employeeArray.length; i++) {
+        // System.out.println(employeeArray[i].name);
+        // }
+
+        // Employee[] employeeArray = new Employee[3];
+
+        // employeeArray[0] = new Employee("Xander", 1);
+        // employeeArray[1] = new Employee("John", 3);
+        // employeeArray[2] = new Employee("Anna", 2);
+
+        // java.util.Arrays.sort(employeeArray, new Comparator<Employee>() {
+        // @Override
+        // public int compare(Employee e1, Employee e2) {
+        // return e1.employeeId - e2.employeeId;
+        // }
+        // });
+
+        // for (int i = 0; i < employeeArray.length; i++) {
+        // System.out.println(employeeArray[i].name);
+        // }
+
+        // java.util.Arrays.sort(employeeArray, new Comparator<Employee>() {
+        // @Override
+        // public int compare(Employee e1, Employee e2) {
+        // int nameDiff = e1.name.compareTo(e2.name);
+        // if (nameDiff != 0) {
+        // return nameDiff;
+        // }
+
+        // return e1.employeeId - e2.employeeId;
+        // }
+        // });
+
+        // Employee[] employeeArray = new Employee[3];
+
+        // employeeArray[0] = new Employee("Xander", 1);
+        // employeeArray[1] = new Employee("John", 3);
+        // employeeArray[2] = new Employee("Anna", 2);
+
+        // java.util.Arrays.sort(employeeArray, new Comparator<Employee>() {
+        // @Override
+        // public int compare(Employee e1, Employee e2) {
+        // return e1.employeeId - e2.employeeId;
+        // }
+        // });
+
+        // for (int i = 0; i < employeeArray.length; i++) {
+        // System.out.println(employeeArray[i].name);
+        // }
+        // java.util.Arrays.sort(employeeArray, new Comparator<Employee>() {
+        // @Override
+        // public int compare(Employee e1, Employee e2) {
+        // int nameDiff = e1.name.compareTo(e2.name);
+        // if (nameDiff != 0) {
+        // return nameDiff;
+        // }
+
+        // return e1.employeeId - e2.employeeId;
+        // }
+        // });
+
+        // int[] intArray = new int[10];
+
+        // Arrays.fill(intArray, 123);
+
+        // System.out.println(Arrays.toString(intArray));
+
+        // int[] intArray = new int[10];
+
+        // Arrays.fill(intArray, 3, 5, 123);
+
+        // System.out.println(Arrays.toString(intArray));
+
+        // int[] ints = { 0, 2, 4, 6, 8, 10 };
+
+        // int index = Arrays.binarySearch(ints, 6);
+
+        // System.out.println(index);
+
+        // int[] ints = { 0, 2, 4, 6, 8, 10 };
+
+        // int index = Arrays.binarySearch(ints, 7);
+
+        // System.out.println(index);
+
+        // int[] ints = { 0, 2, 4, 6, 8, 10 };
+
+        // int index = Arrays.binarySearch(ints, 12);
+
+        // System.out.println(index);
+
+        // int[] ints = { 0, 2, 4, 6, 8, 10 };
+
+        // int index = Arrays.binarySearch(ints, 0, 4, 2);
+
+        // System.out.println(index);
+
+        // int[] ints = { 0, 2, 4, 6, 8, 10 };
+
+        // int index = Arrays.binarySearch(ints, 0, 4, 12);
+
+        int[] ints1 = { 0, 2, 4, 6, 8, 10 };
+        int[] ints2 = { 0, 2, 4, 6, 8, 10 };
+        int[] ints3 = { 10, 8, 6, 4, 2, 0 };
+
+        boolean ints1EqualsInts2 = Arrays.equals(ints1, ints2);
+        boolean ints1EqualsInts3 = Arrays.equals(ints1, ints3);
+
+        System.out.println(ints1EqualsInts2);
+        System.out.println(ints1EqualsInts3);
+
     }
 
 }
