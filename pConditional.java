0e5c73d11ee637cd18b0eb66d9e38a684e585b0e
zeekdocodes <clydeeeee19@gmail.com>
2025-04-05 10:45:57 +0800

    end of conditional statements

---
+++ pConditional.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class pConditional {
+    public static void main(String[] args) {
+
+        /*
+         * if statement syntax
+         * if (boolean expression t or f ){
+         * first function if the expression if true
+         * }else{
+         * function if the expression if false
+         * 
+         * }
+         * 
+         * 
+         */
+
+        // int salary = 20000;
+        // if (salary > 10000) {
+        // salary = salary + 10000;
+
+        // } else {
+
+        // salary = salary + 15000;
+        // }
+        // if (salary >= 20000) {
+        // salary += 5000;
+
+        // } else if (salary >= 15000) {
+        // salary += 7000;
+
+        // } else {
+
+        // salary += 10000;
+
+        // }
+
+        // System.out.println(salary);
+        
+
+        // for (int i = 0; i < 5; i++) {
+
+        //     System.out.println(i);
+        // }
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("count to what number:");
+        // int n = in.nextInt();
+        // System.out.println("couting by what number:");
+        // int c = in.nextInt();
+
+        // for(int num = 1; num < n ; num += c){
+
+        //     System.out.println(num);
+
+            
+       // }
+        //     in.close();
+
+        // int num = 1;
+        // while(num<=5){
+            
+        //     System.out.println(num);
+        //     num +=1;
+
+
+        // }
+
+        // int n =1 ;
+        // do {
+        //     System.out.println(n);
+        //     n++;
+        // }while(n<=5);
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("input three number to see the biggest ");
+        // int a = scanner.nextInt();
+        // int b = scanner.nextInt();
+        // int c = scanner.nextInt();
+
+        // int max = a;
+        // if(b>max){
+        //     max = b;
+        // }
+        // if(c>max){
+        //     max = c;
+        // }
+        // System.out.println(max);
+        // Scanner scanner = new Scanner(System.in);
+
+        // int n = scanner.nextInt();
+        // int a = 0;
+        // int b = 1;
+        // int count = 2; 
+
+        // while(count <=n){
+        //     int temp = b;
+        //     b = b+a;
+        //     a = temp;
+        //     count++;
+            
+
+        // }
+        // System.out.println(b);
+
+
+        // int n = 134335533;
+
+        // int count = 0;
+
+        // while(n>0){
+        //     int rem = n % 10;
+        //     if(rem ==3){
+        //         count++;
+        //     }
+        //     n = n / 10;
+        // }
+        // System.out.println(count);
+        int num = 231423423;
+
+        int ans = 0;
+
+        while(num > 0 ){
+            int rem = num % 10 ;
+            num /=10;
+
+            ans = ans * 10 + rem;
+
+
+
+        }
+
+ System.out.println(ans);
+
+
+    }
+    
+}
